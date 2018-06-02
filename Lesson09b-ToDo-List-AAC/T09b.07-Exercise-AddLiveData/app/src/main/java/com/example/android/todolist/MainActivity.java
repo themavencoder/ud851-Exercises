856e665a4e0f190a67fc8f613800080a98d89c01
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-06-02 20:32:12 +0100

    T09b.07- Adding live data

--- Lesson09b-ToDo-List-AAC/T09b.07-Exercise-AddLiveData/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.07-Exercise-AddLiveData/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,23 +1,26 @@
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.todolist;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -86,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
                         List<TaskEntry> tasks = mAdapter.getTasks();
                         mDb.taskDao().deleteTask(tasks.get(position));
                         // TODO (6) Remove the call to retrieveTasks
-                        retrieveTasks();
+
                     }
                 });
             }
@@ -110,6 +113,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
         mDb = AppDatabase.getInstance(getApplicationContext());
         // TODO (7) Call retrieveTasks from here and remove the onResume method
+        retrieveTasks();
     }
 
     /**
@@ -117,31 +121,27 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
      * Often, this is after new data has been inserted through an AddTaskActivity,
      * so this re-queries the database data for any changes.
      */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        retrieveTasks();
-    }
 
     private void retrieveTasks() {
-        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+
+
+        // TODO (4) Extract all this logic outside the Executor and remove the Executor
+
+        Log.d(TAG, "Actively retrieving the tasks from the DataBase");
+        // TODO (3) Fix compile issue by wrapping the return type with LiveData
+        LiveData<List<TaskEntry>> tasks = mDb.taskDao().loadAllTasks();
+        // TODO (5) Observe tasks and move the logic from runOnUiThread to onChanged
+
+        tasks.observe(this, new Observer<List<TaskEntry>>() {
             @Override
-            public void run() {
-                // TODO (4) Extract all this logic outside the Executor and remove the Executor
-                Log.d(TAG, "Actively retrieving the tasks from the DataBase");
-                // TODO (3) Fix compile issue by wrapping the return type with LiveData
-                final List<TaskEntry> tasks = mDb.taskDao().loadAllTasks();
-                // TODO (5) Observe tasks and move the logic from runOnUiThread to onChanged
-                // We will be able to simplify this once we learn more
-                // about Android Architecture Components
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapter.setTasks(tasks);
-                    }
-                });
+            public void onChanged(@Nullable List<TaskEntry> taskEntries) {
+                mAdapter.setTasks(taskEntries);
             }
         });
+        // We will be able to simplify this once we learn more
+        // about Android Architecture Components
+
+
     }
 
     @Override
