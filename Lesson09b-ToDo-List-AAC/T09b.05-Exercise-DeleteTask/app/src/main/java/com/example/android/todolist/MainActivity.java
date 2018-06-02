e3c6c32055266949565c1b0d2a79ef0b67ec95fe
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-06-02 19:07:58 +0100

    T09b.05-Delete task

--- Lesson09b-ToDo-List-AAC/T09b.05-Exercise-DeleteTask/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.05-Exercise-DeleteTask/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -76,14 +76,24 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
             // Called when a user swipes left or right on a ViewHolder
             @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 // Here is where you'll implement swipe to delete
                 // TODO (1) Get the diskIO Executor from the instance of AppExecutors and
                 // call the diskIO execute method with a new Runnable and implement its run method
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<TaskEntry> taskList = mAdapter.getTasks();
+                        mDb.taskDao().deleteTask(taskList.get(position));
+                        retrieveTasks();
+                    }
+                });
 
                 // TODO (3) get the position from the viewHolder parameter
                 // TODO (4) Call deleteTask in the taskDao with the task at that position
                 // TODO (6) Call retrieveTasks method to refresh the UI
+
             }
         }).attachToRecyclerView(mRecyclerView);
 
@@ -115,6 +125,12 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     protected void onResume() {
         super.onResume();
         // TODO (5) Extract the logic to a retrieveTasks method so it can be reused
+        retrieveTasks();
+
+
+    }
+
+    private void retrieveTasks() {
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
