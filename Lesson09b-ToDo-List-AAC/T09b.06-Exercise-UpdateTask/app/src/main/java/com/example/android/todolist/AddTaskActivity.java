a0974cb8fcaec4a5e5c7ac337d5105174b62977d
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-06-02 20:06:33 +0100

    T09b.06-Update task

--- Lesson09b-ToDo-List-AAC/T09b.06-Exercise-UpdateTask/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.06-Exercise-UpdateTask/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -72,10 +72,23 @@ public class AddTaskActivity extends AppCompatActivity {
             if (mTaskId == DEFAULT_TASK_ID) {
                 // populate the UI
                 // TODO (3) Assign the value of EXTRA_TASK_ID in the intent to mTaskId
+                mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
                 // Use DEFAULT_TASK_ID as the default
 
                 // TODO (4) Get the diskIO Executor from the instance of AppExecutors and
                 // call the diskIO execute method with a new Runnable and implement its run method
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        final TaskEntry taskList = mDb.taskDao().loadTaskById(mTaskId);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                populateUI(taskList);
+                            }
+                        });
+                    }
+                });
 
                 // TODO (5) Use the loadTaskById method to retrieve the task with id mTaskId and
                 // assign its value to a final TaskEntry variable
@@ -115,8 +128,13 @@ public class AddTaskActivity extends AppCompatActivity {
      */
     private void populateUI(TaskEntry task) {
         // TODO (7) return if the task is null
+        if (task == null) {
+            return;
+        }
 
         // TODO (8) use the variable task to populate the UI
+        mEditText.setText(task.getDescription());
+        setPriorityInViews(task.getPriority());
     }
 
     /**
@@ -133,9 +151,16 @@ public class AddTaskActivity extends AppCompatActivity {
             @Override
             public void run() {
                 // TODO (9) insert the task only if mTaskId matches DEFAULT_TASK_ID
+                if (mTaskId == DEFAULT_TASK_ID) {
+                    mDb.taskDao().insertTask(taskEntry);
+
+                } else {
+                        taskEntry.setId(mTaskId);
+                    mDb.taskDao().insertTask(taskEntry);
+                }
                 // Otherwise update it
                 // call finish in any case
-                mDb.taskDao().insertTask(taskEntry);
+
                 finish();
             }
         });
